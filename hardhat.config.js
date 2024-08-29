6ff8c21f476ac75aa05479f92fbf47313255b0b4
rajsha10 <rajsha8617@gmail.com>
2024-08-29 10:27:23 +0530

    contract deployment tested

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,27 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
-require('@nomicfoundation/hardhat-ethers');
-require('hardhat-gas-reporter');
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
-  gasReporter: {
-    enabled: true,
-    currency: 'USD',
-    gasPrice: 21,
+  solidity: "0.8.19",
+  networks: {
+    opencampus: {
+      url: `https://rpc.open-campus-codex.gelato.digital/`,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      opencampus: "your-etherscan-api-key",
+    },
+    customChains: [
+      {
+        network: "opencampus",
+        chainId: 656476,
+        urls: {
+          apiURL: "https://opencampus-codex.blockscout.com/api",
+          browserURL: "https://opencampus-codex.blockscout.com",
+        },
+      },
+    ],
   },
 };
